cc043b46f9edbd331666b3a8d42c8331839636f5
Bahy Adel <bahyadel515@gmail.com>
2025-08-23 23:40:11 +0300

    1

--- backend/app.ts
+++ backend/app.ts
@@ -1,39 +1,49 @@
-import express  from 'express';
-import dotenv from 'dotenv';
-import database from './DB_config/database';
-import AllRoutes from './Routes';
-import cors from 'cors';
-import {errorMiddleware} from './middlwares/globalError';
-import i18nMiddleware from './middlwares/i18n'
-const app : express.Application = express();
+import express from "express";
+import dotenv from "dotenv";
+import database from "./DB_config/database";
+import AllRoutes from "./Routes";
+import cors from "cors";
+import { errorMiddleware } from "./middlwares/globalError";
+import i18nMiddleware from "./middlwares/i18n";
+
+dotenv.config(); 
+
+const app: express.Application = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = ['http://localhost:4200','http://localhost:3000'];
-    
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-app.options('*', cors());
+
+const allowedOrigins = [
+  "http://localhost:4200",
+  "http://localhost:3000",
+  process.env.BASE_URL || ""  
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 database();
-dotenv.config();
 
-app.use("/uploads",express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(i18nMiddleware);
+
+
 AllRoutes(app);
 app.use(errorMiddleware);
 
-app.listen(process.env.PORT ||3010, ()=>{
-   console.log(`App listen on Port : ${process.env.PORT}`) 
-});  
-
-
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => {
+  console.log(`App listening on Port: ${PORT}`);
+});
